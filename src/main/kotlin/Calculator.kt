51c1bbf6362da321184ac70db2de2b4be34085a2
wisedog706 <wisedog706@gmail.com>
2022-04-10 23:32:37 +0500

    getAvailableOperation method implementation added

--- src/main/kotlin/Calculator.kt
+++ src/main/kotlin/Calculator.kt
@@ -1,14 +1,32 @@
+import Operations.Binary.IBinaryOperation
+import Operations.Unary.IUnaryOperation
+import org.reflections.Reflections
+
 object Calculator {
+    private val binaryOperations: HashMap<String, IBinaryOperation> = HashMap()
+    private val unaryOperations: HashMap<String, IUnaryOperation> = HashMap()
+
     init {
+        Reflections("Operations.Binary").getSubTypesOf(IBinaryOperation::class.java)
+            .forEach { x -> val operation: IBinaryOperation = x.getConstructor().newInstance()
+                binaryOperations[operation.name] = operation }
+
+        Reflections("Operations.Unary").getSubTypesOf(IUnaryOperation::class.java)
+            .forEach { x -> val operation: IUnaryOperation = x.getConstructor().newInstance()
+                unaryOperations[operation.name] = operation }
+    }
+
+    private fun inputParser(input: String): List<String>{
         TODO()
     }
 
-    private fun refactorInput(input: String): List<String>{
-        TODO()
-    }
+    fun getAvailableOperations(): HashMap<String, Collection<String>>{
+        val res: HashMap<String, Collection<String>> = HashMap()
 
-    fun getAvailableOperations(): List<String>{
-        TODO()
+        res["binary"] = binaryOperations.keys
+        res["unary"] = unaryOperations.keys
+
+        return res
     }
 
     fun calculate(input: String): Double{
--- src/test/kotlin/CalculatorTest.kt
+++ src/test/kotlin/CalculatorTest.kt
@@ -1,2 +1,14 @@
+import Operations.Binary.AddOperation
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Test
+
 class CalculatorTest {
+    @Test
+    fun testOperationCollector(){
+        val calculator = Calculator
+        val e = calculator.getAvailableOperations()
+
+        assert(e["binary"]!!.contains("+"))
+        assert(e["binary"]!!.contains("*"))
+    }
 }
