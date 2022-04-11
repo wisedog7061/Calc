d4da8e19fed5d78ae7559480c6b87fe7f5d2e615
wisedog706 <wisedog706@gmail.com>
2022-04-11 19:36:48 +0500

    add method main implementation

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,23 @@
 fun main(args: Array<String>){
+    val calculator = Calculator
 
+    if (args.contains("-a")){
+        println(" All Operations: ${calculator.getAvailableOperations()} ")
+        return
+    }
+
+    if(args.contains("-h") || args.contains("--help") || args.isEmpty()){
+        println("Usage: Calculator.jar [OPTIONS] EXPRESSION")
+        println("Example: Calculator.jar 1+2")
+        println()
+        println("Options:")
+        println("   -a      Print all available operations")
+        print("   -h, --help      Print this message")
+
+        return
+    }
+
+    val expression: String = args.joinToString("")
+    println( "Expression: $expression ")
+    println("Result: ${calculator.calculate(expression)}")
 }
