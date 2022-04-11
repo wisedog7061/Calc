4e9c75ac6398480c56755f0ab640ee5310eb7295
wisedog706 <wisedog706@gmail.com>
2022-04-11 18:45:19 +0500

    Calculate method implemented(awful)

--- src/main/kotlin/Calculator.kt
+++ src/main/kotlin/Calculator.kt
@@ -1,6 +1,11 @@
 import Operations.Binary.IBinaryOperation
+import Operations.IOperation
 import Operations.Unary.IUnaryOperation
 import org.reflections.Reflections
+import java.lang.IllegalArgumentException
+import java.util.*
+import kotlin.collections.HashMap
+import kotlin.math.roundToInt
 
 object Calculator {
     private val binaryOperations: HashMap<String, IBinaryOperation> = HashMap()
@@ -8,22 +13,26 @@ object Calculator {
 
     init {
         Reflections("Operations.Binary").getSubTypesOf(IBinaryOperation::class.java)
-            .forEach { x -> val operation: IBinaryOperation = x.getConstructor().newInstance()
-                binaryOperations[operation.name] = operation }
+            .forEach { x ->
+                val operation: IBinaryOperation = x.getConstructor().newInstance()
+                binaryOperations[operation.name] = operation
+            }
 
         Reflections("Operations.Unary").getSubTypesOf(IUnaryOperation::class.java)
-            .forEach { x -> val operation: IUnaryOperation = x.getConstructor().newInstance()
-                unaryOperations[operation.name] = operation }
+            .forEach { x ->
+                val operation: IUnaryOperation = x.getConstructor().newInstance()
+                unaryOperations[operation.name] = operation
+            }
     }
 
-    private fun parseInput(input: String): List<String>{
+    private fun parseInput(input: String): List<String> {
         var result: String = input
 
-        for (operation in binaryOperations.keys){
+        for (operation in binaryOperations.keys) {
             result = result.replace(operation, " $operation ")
         }
 
-        for (operation in unaryOperations.keys){
+        for (operation in unaryOperations.keys) {
             result = result.replace(operation, " $operation ")
         }
 
@@ -35,7 +44,7 @@ object Calculator {
         return result.split(" ")
     }
 
-    fun getAvailableOperations(): HashMap<String, Collection<String>>{
+    fun getAvailableOperations(): HashMap<String, Collection<String>> {
         val res: HashMap<String, Collection<String>> = HashMap()
 
         res["binary"] = binaryOperations.keys
@@ -44,7 +53,87 @@ object Calculator {
         return res
     }
 
-    fun calculate(input: String): Double{
-        TODO()
+    private fun calculateFromStack(numberStack: Stack<Double>, operationName: String) {
+        val operation: IOperation = binaryOperations[operationName]
+            ?: unaryOperations[operationName]!!
+
+        if (operation is IUnaryOperation) {
+            numberStack.add(operation.calculate(numberStack.pop()))
+        } else {
+            val second: Double = numberStack.pop()
+            val first: Double = numberStack.pop()
+
+            numberStack.add((operation as IBinaryOperation).calculate(first, second))
+        }
+    }
+
+    fun calculate(input: String): Double {
+        val splittedSymbols: List<String> = parseInput(input)
+
+        val numberStack: Stack<Double> = Stack()
+        val operationStack: Stack<String> = Stack()
+        try {
+            for (symbol in splittedSymbols) {
+                if (symbol.toDoubleOrNull() != null) {
+                    numberStack.add(symbol.toDouble())
+                    continue
+                }
+
+                if (symbol == "(") {
+                    operationStack.add("(")
+                    continue
+                }
+
+                if (symbol == ")") {
+                    while (!operationStack.empty() && operationStack.peek() != "(") {
+                        calculateFromStack(numberStack, operationStack.pop())
+                    }
+
+                    operationStack.pop()
+                    continue
+                }
+
+                if (symbol in binaryOperations.keys) {
+                    val operation: IBinaryOperation = binaryOperations[symbol]!!
+
+                    if (operationStack.empty() || operationStack.peek() == "(") {
+                        operationStack.add(symbol)
+                        continue
+                    }
+
+                    var temp = operationStack.peek()
+
+                    val operationFromStack: IOperation = binaryOperations[temp]
+                        ?: unaryOperations[temp]!!
+
+                    if (operation.priority > operationFromStack.priority) {
+                        operationStack.add(operation.name)
+                        continue
+                    } else {
+                        while (!operationStack.empty()) {
+                            calculateFromStack(numberStack, temp)
+                            temp = operationStack.pop()
+                            val oper: IOperation = binaryOperations[temp] ?: unaryOperations[temp]!!
+
+                            if (temp == "(" || oper.priority <= oper.priority) {
+                                break
+                            }
+                        }
+
+                        operationStack.add(operation.name)
+                        continue
+                    }
+                }
+            }
+
+            while (!operationStack.empty()) {
+                calculateFromStack(numberStack, operationStack.peek())
+                operationStack.pop()
+            }
+        } catch (e: Exception){
+            throw IllegalArgumentException()
+        }
+
+        return (numberStack.pop() * 1000).roundToInt() / 1000.0
     }
 }
--- src/test/kotlin/CalculatorTest.kt
+++ src/test/kotlin/CalculatorTest.kt
@@ -3,7 +3,9 @@ import org.junit.jupiter.api.Assertions.*
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
 import java.lang.reflect.Method
+import kotlin.math.exp
 
 class CalculatorTest {
     private val calculator: Calculator = Calculator
@@ -43,5 +45,34 @@ class CalculatorTest {
             "2.15", "*", "(", "3",
             "+", "2", ")"
         ), res)
+    }
+
+    @Test
+    fun testCalculateEasyExpression(){
+        val expression: String = "4 + 3 - 1 * 2 / 2"
+
+        assertEquals(6.0, calculator.calculate(expression))
+    }
+
+    @Test
+    fun testCalculateBracketExpression(){
+        val expression: String = "1+2*(3+4/2-(1+2))*2+1"
+
+        assertEquals(10.0, calculator.calculate(expression))
+    }
+
+    @Test
+    fun testCalculateOneNumber(){
+        val expression: String = "5"
+
+        assertEquals(5.0, calculator.calculate(expression))
+    }
+
+    @Test
+    fun testCalculateIncorrectInput(){
+        val expression: String = "5+(4-2"
+
+        val exception = assertThrows(IllegalArgumentException::class.java) { calculator.calculate(expression) }
+        assertEquals(IllegalArgumentException::class, exception::class)
     }
 }
